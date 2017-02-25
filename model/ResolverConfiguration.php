dba3be365f3e47d646937f4d9127bda9ffb766e8
shaunc <ampersign@shaunc.com>
2017-02-25 00:08:03 -0600

    Fucking ^M, dos2unix and reconfigure editor

--- model/ResolverConfiguration.php
+++ model/ResolverConfiguration.php
@@ -22,37 +22,37 @@ class ResolverConfiguration {
     private $name = null;
     private $enabled = false;
     private $filePath = null;
-    private $zoneDefinitionTemplate = null;
+    private $zoneDefinitionTemplate = null;
     
-    public function __construct($name) {
-        $this->name = $name;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-    function setName($name) {
-        $this->name = $name;
-    }
-    public function getEnabled() {
-        return $this->enabled;
-    }
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+    function setName($name) {
+        $this->name = $name;
+    }
+    public function getEnabled() {
+        return $this->enabled;
+    }
     public function isEnabled() {
         return $this->enabled;
     }
-    function setEnabled($enabled) {
-        $this->enabled = $enabled;
-    }
-    public function getFilePath() {
-        return $this->filePath;
-    }
-    function setFilePath($filePath) {
-        $this->filePath = $filePath;
-    }
-    public function getZoneDefinitionTemplate() {
-        return $this->zoneDefinitionTemplate;
-    }
-    function setZoneDefinitionTemplate($zoneDefinitionTemplate) {
-        $this->zoneDefinitionTemplate = $zoneDefinitionTemplate;
-    }
+    function setEnabled($enabled) {
+        $this->enabled = $enabled;
+    }
+    public function getFilePath() {
+        return $this->filePath;
+    }
+    function setFilePath($filePath) {
+        $this->filePath = $filePath;
+    }
+    public function getZoneDefinitionTemplate() {
+        return $this->zoneDefinitionTemplate;
+    }
+    function setZoneDefinitionTemplate($zoneDefinitionTemplate) {
+        $this->zoneDefinitionTemplate = $zoneDefinitionTemplate;
+    }
 }
